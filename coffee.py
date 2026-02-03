5ebdc1763a60b853ec6fdc758e41cbd7c28734de
eman20006 <ef392@kent.ac.uk>
2026-02-03 15:45:04 +0000

    edited version of wkshop5 part2, still not complete

--- coffee.py
+++ coffee.py
@@ -1,6 +1,9 @@
 #Your code goes here
+#might have to try make num 2 a global 
+num = int(input("Please enter change for product: "))
 
 x = 75 
+num = int(input("Please enter change for product: "))
 def change(num):
     if num == 50:
         print("Change accepted, amount due is: ", x - num)
@@ -10,11 +13,11 @@ def change(num):
         print("Change accepted, amount due is: ", x - num)
     if num == 5:
         print("Change accepted, amount due is: ", x - num)
-
+   
 def change(num2):
+     
      if num2 == 50:
         print("Change accepted, amount due is: ", x - num - num2)
-
      if num2 == 25:
         print("Change accepted, amount due is: ", x - num - num2)
      if num2 == 10:
@@ -25,6 +28,7 @@ def change(num2):
 def main():
     num = int(input("Please enter change for product: "))
     change(num)
-    num2 = int(input("Please enter change for product: "))
+    change(num2)
+    
 
 main()
