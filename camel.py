270191d30e25028eec183cfa2da1304f9e2ecc66
eman20006 <ef392@kent.ac.uk>
2026-02-03 13:14:18 +0000

    workshop 5 task 1

--- camel.py
+++ camel.py
@@ -1,1 +1,20 @@
+
+
 # Enter your code here
+
+#user inputs say ethanAlexanderFilmer, wanna make that into ethan_alexander_filmer
+#str_name = str(name)
+
+def to_snakecase(str_name):
+    for one_char in str_name:
+        if one_char.islower():
+         print(one_char,end='')
+        if one_char.isupper():
+         print('_' + one_char.lower(),end='')
+
+def main():
+    name = input("What's your full name? ")
+
+    to_snakecase(name)
+
+main()    
